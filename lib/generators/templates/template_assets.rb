54c049688ae3f5e7682a203fc239867413152431
Pedro de Assis <pedro@caiena.net>
2015-11-18 16:15:10 -0200

    updating initializer template

--- lib/generators/templates/template_assets.rb
+++ lib/generators/templates/template_assets.rb
@@ -8,3 +8,18 @@
 
 # precompiling template/action assets
 Rails.application.config.assets.precompile << /.*\/?(index|new|show|edit).*\z/
+
+TemplateAssets.configure do |config|
+  #   This is where TemplateAssets gets initialized and you can customize it
+  # to your needs.
+  #
+  #
+  #   Here you can override TemplateAssets caching.
+  #   Example using memcached:
+  #
+  # config.cache_store = ActiveSupport::Cache::MemCacheStore.new(
+  #   'localhost',
+  #   'server-downstairs.localnetwork:8229'
+  # )
+  #
+end
